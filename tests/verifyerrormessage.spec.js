b72635b69b5e4b9cd930b92ff7b449982a7c39cc
amjathasyoulike <amjathkrm2021@gmail.com>
2025-09-08 11:13:40 +0530

    test2

--- tests/verifyerrormessage.spec.js
+++ tests/verifyerrormessage.spec.js
@@ -8,8 +8,9 @@ test("verifithe error in the page", async ({page}) => {
     await page.locator("input[name='password']").fill("amin123")
      await page.locator("button[type='submit']").click()
      const errormessage = await page.locator('xpath=//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p').textContent()
-
+    
      console.log(errormessage)
+     await page.close()
     
 })  
 
